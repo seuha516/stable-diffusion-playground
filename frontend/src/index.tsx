b5cefd092ced6737e67d229a64e200141ba30db6
Seungha Jeon <seuha516@naver.com>
2023-09-12 15:19:41 +0900

    create index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
+import "./index.scss";
+import MainPage from "./view/main-page";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -8,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>
 );
