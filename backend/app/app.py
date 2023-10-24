8d1282e289c019f2c1bb9038ed1be1b53baf61af
Minsoo Cheong <icycle0409@snu.ac.kr>
2023-10-24 20:25:39 +0900

    fix gunicorn setting, mount model volume

--- backend/app/app.py
+++ backend/app/app.py
@@ -7,63 +7,66 @@ import json
 import config
 # import storage
 
-app = Flask(__name__)
-app.config.from_object(config.LocalConfig)
-CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(cors_allowed_origins="*")
+
+def create_app():
+    
+    app = Flask(__name__)
+    app.config.from_object(config.LocalConfig)
+    CORS(app)
+    
+    # Attach the app to the global socketio object
+    socketio.init_app(app)
+
+    @app.route('/health', methods=['GET'])
+    def health():
+        return 'OK'
 
 
-@app.route('/health', methods=['GET'])
-def health():
-    return 'OK'
+    @app.route('/images/<image_filename>', methods=['GET'])
+    def get_images(image_filename):
+        try:
+            return send_from_directory('storage', image_filename)
+        except Exception as e:
+            return str(e)
 
 
-@app.route('/images/<image_filename>', methods=['GET'])
-def get_images(image_filename):
-    try:
-        return send_from_directory('storage', image_filename)
-    except Exception as e:
-        return str(e)
+    @app.route('/v1/predictions', methods=['POST'])
+    def predictions():
+        input_data = json.loads(request.form['data'])
 
+        # for txt2img
+        prompt = input_data.get('prompt', '')
+        negative_prompt = input_data.get('negative_prompt', '')
+        width = int(input_data.get('width', 1024))
+        height = int(input_data.get('height', 1024))
+        num_outputs = int(input_data.get('num_outputs', 1))
+        num_inference_steps = int(input_data.get('num_inference_steps', 50))
+        guidance_scale = float(input_data.get('guidance_scale', 7.5))
+        scheduler = input_data.get('scheduler', 'K_EULER')
+        seed = int(input_data.get('seed', -1))
 
-@app.route('/v1/predictions', methods=['POST'])
-def predictions():
-    input_data = json.loads(request.form['data'])
+        # for img2img
+        image = request.files.get('image', None)
+        if image:
+            image = Image.open(image).convert('RGB')
+        prompt_strength = float(input_data.get('prompt_strength', 0.8))
 
-    # for txt2img
-    prompt = input_data.get('prompt', '')
-    negative_prompt = input_data.get('negative_prompt', '')
-    width = int(input_data.get('width', 1024))
-    height = int(input_data.get('height', 1024))
-    num_outputs = int(input_data.get('num_outputs', 1))
-    num_inference_steps = int(input_data.get('num_inference_steps', 50))
-    guidance_scale = float(input_data.get('guidance_scale', 7.5))
-    scheduler = input_data.get('scheduler', 'K_EULER')
-    seed = int(input_data.get('seed', -1))
+        predict(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            width=width,
+            height=height,
+            num_outputs=num_outputs,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            scheduler=scheduler,
+            seed=seed,
+            image=image,
+            prompt_strength=prompt_strength,
+            socketio=socketio,
+        )
 
-    # for img2img
-    image = request.files.get('image', None)
-    if image:
-        image = Image.open(image).convert('RGB')
-    prompt_strength = float(input_data.get('prompt_strength', 0.8))
-
-    predict(
-        prompt=prompt,
-        negative_prompt=negative_prompt,
-        width=width,
-        height=height,
-        num_outputs=num_outputs,
-        num_inference_steps=num_inference_steps,
-        guidance_scale=guidance_scale,
-        scheduler=scheduler,
-        seed=seed,
-        image=image,
-        prompt_strength=prompt_strength,
-        socketio=socketio,
-    )
-
-    return jsonify(success=True)
-
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+        return jsonify(success=True)
+    
+    return app
