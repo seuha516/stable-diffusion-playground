c7b536ce30092d8e7b43e27b41229c6ed6c586f0
Seungha Jeon <seungha@friendli.ai>
2023-10-28 14:55:54 +0900

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,18 +1,18 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, session, send_from_directory
 from flask_cors import CORS
-from flask_socketio import SocketIO
-from PIL import Image
+from flask_socketio import SocketIO, join_room
 from predict import predict
-import json
+from PIL import Image
 import torch
-# import storage
 import config
 import const
+import util
+# import storage
 
 app = Flask(__name__)
 app.config.from_object(config.LocalConfig)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, async_mode="threading", cors_allowed_origins="*")
 
 
 @app.route('/health', methods=['GET'])
@@ -25,58 +25,84 @@ def gpu():
     return 'True' if torch.cuda.is_available() else 'False'
 
 
-@app.route('/socket-test', methods=['POST'])
-def socket_test():
-    socketio.emit("test", "socket test")
+@socketio.on('connect')
+def connect():
+    room = util.get_random_string()
+    session['room'] = room
+    join_room(room)
 
-    return 'OK'
+    print(f'Connect {room}')
+    socketio.emit('message', f'Connect {room}', room=room)
 
 
-@app.route(f'{const.GET_IMAGE_API_PATH}/<image_filename>', methods=['GET'])
-def get_images(image_filename):
+@socketio.on('disconnect')
+def disconnect():
+    room = session['room']
+    session.clear()
+
+    print(f'Disconnect {room}')
+
+
+@socketio.on("request")
+def socket_request(message):
+    room = session['room']
+    print(f'(Socket request from {room})', message)
+
+    body = message['body']
+
+    if message['type'] == 'prediction':
+        # for txt2img
+        prompt = body.get('prompt', '')
+        negative_prompt = body.get('negative_prompt', '')
+        width = int(body.get('width', 1024))
+        height = int(body.get('height', 1024))
+        num_outputs = int(body.get('num_outputs', 1))
+        num_inference_steps = int(body.get('num_inference_steps', 50))
+        guidance_scale = float(body.get('guidance_scale', 7.5))
+        scheduler = body.get('scheduler', 'K_EULER')
+        seed = int(body.get('seed', -1))
+
+        # for img2img
+        image_name = request.files.get('image', None)
+        image = None
+        if image_name:
+            image = Image.open(f'{const.STORAGE_DIR_PATH}/{image_name}').convert('RGB')
+        prompt_strength = float(body.get('prompt_strength', 0.8))
+
+        predict(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            width=width,
+            height=height,
+            num_outputs=num_outputs,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            scheduler=scheduler,
+            seed=seed,
+            image=image,
+            prompt_strength=prompt_strength,
+            socketio=socketio,
+            room=room
+        )
+    else:
+        print('Not found')
+
+
+@app.route(f'{const.IMAGE_API_PATH}/<image_filename>', methods=['GET'])
+def get_image(image_filename):
     try:
-        return send_from_directory('storage', image_filename)
+        return send_from_directory(const.STORAGE_DIR_PATH, image_filename)
     except Exception as e:
         return str(e)
 
 
-@app.route('/v1/predictions', methods=['POST'])
-def predictions():
-    input_data = json.loads(request.form['data'])
-
-    # for txt2img
-    prompt = input_data.get('prompt', '')
-    negative_prompt = input_data.get('negative_prompt', '')
-    width = int(input_data.get('width', 1024))
-    height = int(input_data.get('height', 1024))
-    num_outputs = int(input_data.get('num_outputs', 1))
-    num_inference_steps = int(input_data.get('num_inference_steps', 50))
-    guidance_scale = float(input_data.get('guidance_scale', 7.5))
-    scheduler = input_data.get('scheduler', 'K_EULER')
-    seed = int(input_data.get('seed', -1))
-
-    # for img2img
+@app.route(f'{const.IMAGE_API_PATH}', methods=['POST'])
+def set_image():
     image = request.files.get('image', None)
-    if image:
-        image = Image.open(image).convert('RGB')
-    prompt_strength = float(input_data.get('prompt_strength', 0.8))
+    image = Image.open(image).convert('RGB')
+    image_name = util.save_image(image).get("image_name")
 
-    predict(
-        prompt=prompt,
-        negative_prompt=negative_prompt,
-        width=width,
-        height=height,
-        num_outputs=num_outputs,
-        num_inference_steps=num_inference_steps,
-        guidance_scale=guidance_scale,
-        scheduler=scheduler,
-        seed=seed,
-        image=image,
-        prompt_strength=prompt_strength,
-        socketio=socketio,
-    )
-
-    return 'OK'
+    return image_name
 
 
 if __name__ == '__main__':
