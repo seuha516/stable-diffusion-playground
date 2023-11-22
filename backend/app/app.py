c43f36e4682aa7956388038f6093504afa677f4c
Minsoo Cheong <icycle0409@snu.ac.kr>
2023-11-22 17:31:50 +0900

    use 8000 port

--- backend/app/app.py
+++ backend/app/app.py
@@ -125,4 +125,4 @@ def similar_prompt(prompt):
     return milvus.find_similar_images_by_prompt(prompt)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
--- backend/app/const.py
+++ backend/app/const.py
@@ -1,5 +1,5 @@
 IS_LIGHT_MODEL = True
-SERVEL_URL = 'http://localhost:5000'
+SERVEL_URL = 'http://localhost:8000'
 STORAGE_DIR_PATH = './storage'
 IMAGE_API_PATH = '/image'
 MILVUS_HOST = 'standalone'
