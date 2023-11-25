62dd3a66480a5f68942b6f34298725ec7dc4c082
Seungha Jeon <seungha@friendli.ai>
2023-11-25 17:32:12 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,12 +10,18 @@ for arg in "$@"; do
   fi
 done
 
-DOCKER_VOLUME_DIRECTORY=./frontend docker-compose --project-directory ./frontend -f frontend/docker-compose.yml up &
+(
+  cd ./frontend
+  docker-compose --project-directory . -f docker-compose.yml up
+) &
 
-if nvidia-smi &> /dev/null; then
-  echo "GPU detected. Running with GPU support."
-  DOCKER_VOLUME_DIRECTORY=./backend docker-compose --project-directory ./backend -f backend/docker/gpu/docker-compose.yml up $BUILD_FLAG
-else
-  echo "No GPU detected. Running without GPU support."
-  DOCKER_VOLUME_DIRECTORY=./backend docker-compose --project-directory ./backend -f backend/docker/nogpu/docker-compose.yml up $BUILD_FLAG
-fi
+(
+  cd ./backend
+  if nvidia-smi &> /dev/null; then
+    echo "GPU detected. Running with GPU support."
+    DOCKER_VOLUME_DIRECTORY=. docker-compose --project-directory . -f docker/gpu/docker-compose.yml up $BUILD_FLAG
+  else
+    echo "No GPU detected. Running without GPU support."
+    DOCKER_VOLUME_DIRECTORY=. docker-compose --project-directory . -f docker/nogpu/docker-compose.yml up $BUILD_FLAG
+  fi
+)
